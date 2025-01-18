937f6cf0289398020cf56593c7a0105ef070eefe
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-18 18:52:04 -0500

    Repositorio para clase Activity creado

---
+++ src/main/java/com/samuel/giftcard_activites/Activiy/ActivityRepository.java
@@ -0,0 +1,7 @@
+package com.samuel.giftcard_activites.Activiy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, Integer>{
+
+}
