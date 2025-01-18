312e2714ae8ff25d14d3e9e248b38e217e4aab09
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-18 18:52:52 -0500

    Anotacion repository agregada

--- src/main/java/com/samuel/giftcard_activites/Activiy/ActivityRepository.java
+++ src/main/java/com/samuel/giftcard_activites/Activiy/ActivityRepository.java
@@ -1,7 +1,9 @@
 package com.samuel.giftcard_activites.Activiy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Activity, Integer>{
 
 }
