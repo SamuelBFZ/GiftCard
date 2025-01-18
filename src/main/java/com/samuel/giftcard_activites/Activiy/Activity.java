4bce426b09999b74756e5d6210fe178ad2f00f75
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-17 19:05:46 -0500

    hibernate de properties actualizado, entidad Activity creada, actualizacion en entidad GiftCar y relacion creada

---
+++ src/main/java/com/samuel/giftcard_activites/Activiy/Activity.java
@@ -0,0 +1,28 @@
+package com.samuel.giftcard_activites.Activiy;
+
+import com.samuel.giftcard_activites.Giftcard.GiftCard;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer duration;
+
+    @ManyToOne(targetEntity = GiftCard.class)
+    private GiftCard giftCard;
+}
--- src/main/java/com/samuel/giftcard_activites/Giftcard/GiftCard.java
+++ src/main/java/com/samuel/giftcard_activites/Giftcard/GiftCard.java
@@ -1,10 +1,19 @@
 package com.samuel.giftcard_activites.Giftcard;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.samuel.giftcard_activites.Activiy.Activity;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,5 +29,9 @@ public class GiftCard {
     private String title;
     private String recipientName; //Nombre del receptor
     private String carrierName; //Nombre del emisor 
-    private LocalDate createdDate;
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @OneToMany(targetEntity = Activity.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Activity> activities;
 }
